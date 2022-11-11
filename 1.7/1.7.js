245d769770b1abb72a84b5fac8ea421e29b3fbea
samibarbaglia <sofia.barbaglia@metropolia.fi>
2022-11-11 20:24:36 +0200

    mod

---
+++ 1.7/1.7.js
@@ -0,0 +1,9 @@
+let dice_amount = parseInt(prompt
+('Input number of dices.'))
+let sum = 0
+for (let v = 0; v !== dice_amount; v++)
+{
+    let dice_total = Math.floor(Math.random()*6)+1
+    sum = sum + dice_total
+}
+kohde.innerHTML = 'Result of the dices: ' + sum;
